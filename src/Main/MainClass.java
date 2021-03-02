6ed7940a946ed7a789ec60d3785a6f7573e5b6a6
yejin <yejin@naver.com>
2021-03-02 19:47:23 +0900

    기본 프로젝트 생성

---
+++ src/Main/MainClass.java
@@ -0,0 +1,21 @@
+package Main;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("main인 시작");
+		Scanner input = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.로그인 2.회원관리");
+			num = input.nextInt();
+			switch(num) {
+			case 1 : break;
+			case 2 : break;
+			}
+		}
+	}
+
+}
